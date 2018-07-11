85731f58191add4082c42cf844e765f794076d0c
zhuzhenguang <zhuzhenguang2008@gmail.com>
2018-07-12 00:48:55 +0800

    first test

--- src/test/java/org/maze/MazeTest.java
+++ src/test/java/org/maze/MazeTest.java
@@ -6,7 +6,17 @@ import static org.junit.Assert.assertEquals;
 
 public class MazeTest {
     @Test
-    public void test() {
-        assertEquals(1, 1);
+    public void should_init_road_grids() {
+        Maze maze = new Maze();
+
+        String result = maze.print("3 3");
+
+        assertEquals("[W] [W] [W] [W] [W] [W] [W]\n" +
+                "[W] [R] [W] [R] [W] [R] [W]\n" +
+                "[W] [W] [W] [W] [W] [W] [W]\n" +
+                "[W] [R] [W] [R] [W] [R] [W]\n" +
+                "[W] [W] [W] [W] [W] [W] [W]\n" +
+                "[W] [R] [W] [R] [W] [R] [W]\n" +
+                "[W] [W] [W] [W] [W] [W] [W]\n", result);
     }
 }
