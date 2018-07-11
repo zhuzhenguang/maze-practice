979a1f5b3643c6449cd1ec34f9d489463f0de307
Zhuzhenguang <zhuzhenguang2008@gmail.com>
2018-07-11 18:36:24 +0800

    init project

---
+++ src/test/java/org/maze/MazeTest.java
@@ -0,0 +1,12 @@
+package org.maze;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class MazeTest {
+    @Test
+    public void test() {
+        assertEquals(1, 1);
+    }
+}
